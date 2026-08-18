4422655e27c01443eed9951136f9cac03f33605f
tharindu899 <prabath99t@gmail.com>
2026-08-18 13:29:34 +0530

    -vt fix

--- bot/helper/video_utils/selector.py
+++ bot/helper/video_utils/selector.py
@@ -36,7 +36,6 @@ class SelectMode:
         self.message_event = Event()
         self.is_cancelled = False
 
-    @new_task
     async def _event_handler(self):
         pfunc = partial(cb_vidtools, obj=self)
         handler = self.listener.client.add_handler(
